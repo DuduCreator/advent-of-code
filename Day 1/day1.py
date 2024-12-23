ed9398298edf3e26ab1d87a31953aad6988731d6
DuduCreator <117525771+DuduCreator@users.noreply.github.com>
2024-12-23 13:46:21 +0200

    Update day1.py
    final

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,19 +1,32 @@
 right = []
 left = []
-res = 0
+counter = {}
+res1 = 0
+res2 = 0
 
+# Part 1
 with open("input.txt", "r") as file:
     for line in file:
-        l, r = line.split("    ")
+        l, r = line.split()
         l = int(l)
         r = int(r)
         left.append(l)
         right.append(r)
+        counter[r] = counter[r] + 1 if r in counter else 1
 
-left = left.sort()
-right = right.sort()
+left.sort()
+right.sort()
 
 for i in range(len(left)):
-    res += abs(left[i] - right[i])
+    res1 += abs(left[i] - right[i])
 
-print(res)
+print(f"Part 1: {res1}")
+
+# Part 2
+for i in left:
+    try:
+        res2 += i * counter[i]
+    except KeyError:
+        pass 
+
+print(f"Part 2: {res2}")
