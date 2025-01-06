52bc61cebbd63caeb83888b97838b8110a9abf42
DuduCreator <117525771+DuduCreator@users.noreply.github.com>
2025-01-06 12:35:58 +0200

    Add part 1 solution

--- Day3/part1.py
+++ Day3/part1.py
@@ -1,1 +1,13 @@
+import re
+ans = 0
 
+with open("input.txt", "r") as f:
+    file = f.read().strip()
+
+matches = re.findall(r"mul\((\d{1,3},\d{1,3})\)", file)
+
+for match in matches:
+    a, b = map(int, match.split(","))
+    ans += a * b
+
+print(ans)
