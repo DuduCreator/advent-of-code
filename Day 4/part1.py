02a1199b218056a08928a198db13d157279cfdd9
DuduCreator <117525771+DuduCreator@users.noreply.github.com>
2025-01-06 23:09:36 +0200

    11:09 PM 1/6/2025

--- Day 4/part1.py
+++ Day 4/part1.py
@@ -2,19 +2,21 @@
 
 import re
 
-def check_horizontal():
+def check_horizontal(file):
+    re.search("XMAS", file) # not done
+    ...
+
+def check_vertical(file):
     ...
 
 
-def check_vertical():
-    ...
-
-
-def check_diagonal():
+def check_diagonal(file):
     ...
 
 def main():
-    ...
+    with open("input.txt", "r") as f:
+        file = f.read().strip().split("\n")
+        ...
 
 if __name__ == "__main__":
     main()
