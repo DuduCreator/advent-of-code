f8f5ead1e4e4c5725fc800e834aae05731902155
DuduCreator <117525771+DuduCreator@users.noreply.github.com>
2025-01-06 14:05:15 +0200

    Add part 1 work in progress solution

---
+++ Day 4/part1.py
@@ -0,0 +1,20 @@
+### Work in progress
+
+import re
+
+def check_horizontal():
+    ...
+
+
+def check_vertical():
+    ...
+
+
+def check_diagonal():
+    ...
+
+def main():
+    ...
+
+if __name__ == "__main__":
+    main()
