62292b25fd7980b1fea3e244a085793f4521f437
DuduCreator <117525771+DuduCreator@users.noreply.github.com>
2025-01-09 18:41:27 +0000

    Implement horizontal and vertical checks in part 1 solution

--- Day 4/part1.py
+++ Day 4/part1.py
@@ -2,21 +2,36 @@
 
 import re
 
-def check_horizontal(file):
-    re.search("XMAS", file) # not done
+def check_horizontal(lines):
+    ret = 0
+    for line in lines:
+        if match := re.findall("XMAS", line):
+            ret += len(match)
+    return ret
+
+
+def check_vertical(lines):
+    ret = 0
+    for i in range(len(lines[0])):
+        for j in range(len(lines)):
+            if lines[j][i] == "X" and lines[j+1][i] == "M" and lines[j+2][i] == "A" and lines[j+3][i] == "S":
+                ret += 1
+    return ret
+
+
+def check_diagonal(lines):
     ...
 
-def check_vertical(file):
-    ...
-
-
-def check_diagonal(file):
-    ...
 
 def main():
-    with open("input.txt", "r") as f:
-        file = f.read().strip().split("\n")
-        ...
+    ans = 0
+    with open("input.txt", "r") as file:
+        lines = file.read().strip().split("\n")
+
+    ans += check_horizontal(lines)
+    ans += check_vertical(lines)
+    print(ans)
+
 
 if __name__ == "__main__":
     main()
