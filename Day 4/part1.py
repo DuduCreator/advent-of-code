a86936d9791d8e778a887af9bc66898b9e67af3d
DuduCreator <117525771+DuduCreator@users.noreply.github.com>
2025-01-10 16:23:30 +0000

    Almost done with part 1

--- Day 4/part1.py
+++ Day 4/part1.py
@@ -1,9 +1,12 @@
 import re
 
+
 def check_horizontal(lines):
     ret = 0
     for line in lines:
         if match := re.findall("XMAS", line):
+            ret += len(match)
+        if match := re.findall("XMAS", line[::-1]):
             ret += len(match)
     return ret
 
@@ -14,14 +17,22 @@ def check_vertical(lines):
         for j in range(len(lines)-3):
             if lines[j][i] == "X" and lines[j+1][i] == "M" and lines[j+2][i] == "A" and lines[j+3][i] == "S":
                 ret += 1
+            if lines[j][i] == "S" and lines[j+1][i] == "A" and lines[j+2][i] == "M" and lines[j+3][i] == "X":
+                ret += 1
     return ret
 
 
 def check_diagonal(lines):
     ret = 0 
-    for i in range(len(lines[0])-3):
+    for i in range(len(lines[0])):
         for j in range(len(lines)-3):
-            if lines[j][i] == "X" and lines[j+1][i+1] == "M" and lines[j+2][i+2] == "A" and lines[j+3][i+3] == "S":
+            if i + 3 < len(lines[0]) and lines[j][i] == "X" and lines[j+1][i+1] == "M" and lines[j+2][i+2] == "A" and lines[j+3][i+3] == "S":
+                ret += 1
+            if i + 3 < len(lines[0]) and lines[j][i] == "S" and lines[j+1][i+1] == "A" and lines[j+2][i+2] == "M" and lines[j+3][i+3] == "X":
+                ret += 1
+            if i - 3 > 0 and lines[j][i] == "X" and lines[j+1][i-1] == "M" and lines[j+2][i-2] == "A" and lines[j+3][i-3] == "S":
+                ret += 1
+            if i - 3 > 0 and lines[j][i] == "S" and lines[j+1][i-1] == "A" and lines[j+2][i-2] == "M" and lines[j+3][i-3] == "X":
                 ret += 1
     return ret
 
@@ -31,9 +42,7 @@ def main():
     with open("input.txt", "r") as file:
         lines = file.read().strip().split("\n")
 
-    ans += check_horizontal(lines)
-    ans += check_vertical(lines)
-    ans += check_diagonal(lines)
+    ans = check_horizontal(lines) + check_vertical(lines) + check_diagonal(lines)
     print(ans)
 
 
