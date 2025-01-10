7ed8f611829573c9c42816dd6e684d67f7d6ba45
DuduCreator <117525771+DuduCreator@users.noreply.github.com>
2025-01-10 18:20:18 +0000

    Add part2/Day4 almost complete

--- Day 4/part2.py
+++ Day 4/part2.py
@@ -1,9 +1,16 @@
-def x_max():
-    ...
+def x_max(lines):
+    for i, line in enumerate(lines):
+        for j, char in enumerate(line):
+            if char == "A" and ((lines[i-1][j-1] == "M" and lines[i+1][j+1] == "S") or (lines[i-1][j-1] == "S" and lines[i+1][j+1] == "M")) \
+            and ((lines[i-1][j+1] == "M" and lines[i+1][j-1] == "S") or (lines[i-1][j+1] == "D" and lines[i+1][j-1] == "M")):
+                return 1
 
 
 def main():
-    ...
+    with open("input.txt", "r") as file:
+        lines = file.read().strip().split("\n")
+
+        print(sum(x_max))
 
 
 if __name__ == "__main__":
