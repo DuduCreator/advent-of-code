3921797634cb0c88fec7ee2a1b0986ba3a11dc48
DuduCreator <117525771+DuduCreator@users.noreply.github.com>
2025-02-28 19:09:08 +0000

    Add Day 5 part 1 solution

--- Day 5/part1.py
+++ Day 5/part1.py
@@ -1,10 +1,37 @@
 import re
 
-def main():
-    with open("input.txt", "r") as file:
-        lines = file.read().strip().split("\n")
 
-    print(lines)
+def main():
+    good_rows = []
+    with open("./Day 5/input.txt", "r") as file:
+        rules, updates = file.read().strip().split("\n\n")
+    updates = updates.split("\n")
+
+    for pages in updates:
+        if check_page(pages, rules):
+            good_rows.append(pages)
+
+    print(answer(good_rows))
+
+
+def answer(good_rows):
+    answer = 0
+    for pages in good_rows:
+        pages = pages.split(",")
+        middle = pages[int((len(pages)-1)/2)]
+        answer += int(middle)
+    return answer
+
+
+
+def check_page(pages, rules):
+    pages = pages.split(",")
+    for index, page in enumerate(pages[1:]):
+        matches = re.findall(rf"{page}\|(\d*)\n", rules)
+        for match in matches:
+            if match in pages[:index+1]:
+                return False
+    return True
 
 
 if __name__ == "__main__":
